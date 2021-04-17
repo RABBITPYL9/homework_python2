ec0f85dbb9741b83e57b406042c26d111a82c45b
RABBITPYL9 <shubins317@gmail.com>
2021-04-17 18:38:31 +0300

    ver 1 top7 browsers

--- hw8.py
+++ hw8.py
@@ -5,18 +5,51 @@ open_log = pandas.read_excel('logs.xlsx', sheet_name='log')
 log_data_dict = open_log.to_dict(orient='records')
 slovar = log_data_dict
 slov_rez = {}
+top1 = {}
+top2 = {}
+top3 = {}
+top4 = {}
+top5 = {}
+top6 = {}
+top7 = {}
 for element in slovar:
     slovar = element['Браузер']#создаем список в словарь со всеми браузерами
     if slovar in slov_rez.keys(): 
         slov_rez[slovar] += 1
+        for k, v in slov_rez.items():
+            if v > 450:
+                #print(k)
+                top1 = k
+            elif v > 240:
+                
+                top2 = k
+            elif v > 185:
+
+                top3 = k
+            elif v > 160:
+                
+                top4 = k
+            elif v > 143:
+                
+                top5 = k
+            elif v > 90:
+                
+                top6 = k
+            elif v > 50:
+
+                top7 = k
+                
+
+        
     else:
         slov_rez[slovar] = 1
 
-maximum = 0
-item_title = ''
-for k, v in slov_rez.items():
-    if v > maximum:
-        maximum = v
-        item_title = k
-print(f'Самый популярный браузер: {item_title}')
+print(top1)
+print(top2)
+print(top3)
+print(top4)
+print(top5)
+print(top6)
+print(top7)
+
 
